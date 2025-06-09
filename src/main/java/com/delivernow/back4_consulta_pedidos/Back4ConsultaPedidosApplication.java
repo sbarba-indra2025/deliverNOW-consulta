1436ad24375cde414e553aba5ad6086e183748db
sbarba-indra2025 <sbarba-indra2025@gmail.com>
2025-06-09 20:17:25 +0200

    .

---
+++ src/main/java/com/delivernow/back4_consulta_pedidos/Back4ConsultaPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.delivernow.back4_consulta_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back4ConsultaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Back4ConsultaPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivernow/back4_consulta_pedidos/Back4ConsultaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivernow.back4_consulta_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back4ConsultaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
